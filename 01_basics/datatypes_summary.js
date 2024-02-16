cee94283f973238104de139794a1e78ad502324c
Saniya Khan <saniyakhanjlp@gmail.com>
2024-02-16 17:23:34 +0000

    datatypes

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,38 @@
+//*** Based on the access and storage in memory datatypes are classified in 2 types */
+//1st PRIMITIVE //
+
+
+// 7 types:  string, number, null, undefined, symbol, bigInt, boolean
+const score = 100
+const scorevalue = 100.4
+
+const isLoggedIn = false
+const outsideTemp = null
+let usermail;
+ 
+//sybol uniquely identifies the variables
+const id = Symbol("123")
+const otherid = Symbol("123")
+console.log(id == otherid);  //false
+
+const bigNum = 3348459589456384n
+console.log(typeof bigNum);  //bigint
+//2nd Reference (NON PRIMITIVE)
+
+// typeof(null) ==> object
+//typeof(undefined) ==> undefined
+//returntype of non primitive gives object on typeof and of function() is function
+//Array, Objects, Functions
+
+const heros = ["Shaktiman", "Naagraj","Doga"];
+
+let myObj = {
+    name: "saniya",
+    age: 20,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+// https://tc39.es/ecma262/#sec-ecmascript-language-types
